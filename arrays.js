b7b1d34d1cd42b0f33971d9dfab882098e86fda2
Chibueze Osuoji <chibz2006@yahoo.com>
2019-01-31 18:00:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,36 +1,6 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-
-var = grilledCheeseIngredients = [
-
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic',
-]
-
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-]
-
-var chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles',
-]
-
-
-var superheroines = ["catwoman", "she-hulk", "mystique"];
-
-superheroines.push("wonder woman");
+function addElementToBeginningOfArray(array, element)
+{
+  array = [element, ...array]
+  array;
+  return array;
+}
